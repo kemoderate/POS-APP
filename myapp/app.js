5f7848c3de89521f297eebec7290249789ab4dad
fahmi <fahminomana@gmail.com>
2023-07-05 09:33:56 +0700

    login beres

---
+++ myapp/app.js
@@ -0,0 +1,119 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'posdb',
+  password: '12345',
+  port: 5432,
+});
+
+const indexRouter = require('./routes/index')(pool);
+const usersRouter = require('./routes/users');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'fahmi',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var debug = require('debug')('myapp:server');
+var http = require('http');
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = app;
---
+++ myapp/routes/index.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+const saltRounds = 10;
+
+
+
+module.exports = (pool) => {
+  function requireAuth(req, res, next) {
+    if (req.session.authenticated) {
+      next(); // User is authenticated, continue to the next middleware/route handler
+    } else {
+      res.redirect('/login'); // User is not authenticated, redirect to the login page
+    }
+  }
+
+  router.get('/login', function (req, res, next) {
+    if (req.session.authenticated) {
+      res.render('index', { title: 'index' });
+    } else {
+      res.render('login', { title: 'Dashboard', showLoginForm: true });
+    }
+  });
+
+  router.post('/login', function (req, res, next) {
+    const { email, password } = req.body;
+    pool.query('SELECT * FROM users WHERE email = $1', [email], (error, result) => {
+      if (error) {
+        console.error('Error executing query', error);
+        res.render('login', { title: 'Dashboard', showLoginForm: true, error: 'An error occurred. Please try again later.' });
+      } else {
+        if (result.rowCount === 1) {
+          const user = result.rows[0];
+          bcrypt.compare(password, user.password, (bcryptError, bcryptResult) => {
+            if (bcryptError) {
+              console.error('Error comparing passwords', bcryptError);
+              res.render('login', { title: 'Dashboard', showLoginForm: true, error: 'An error occurred. Please try again later.' });
+            } else {
+              if (bcryptResult) {
+                req.session.authenticated = true;
+                req.session.user = {
+                  name: user.name // Store the name field in the session
+              };
+                res.redirect('/');
+              } else {
+                res.render('login', { title: 'Dashboard', showLoginForm: true, error: 'Invalid email or password' });
+              }
+            }
+          });
+        } else {
+          res.render('login', { title: 'Dashboard', showLoginForm: true, error: 'Invalid email or password' });
+        }
+      }
+    });
+  });
+
+  router.get('/', requireAuth, (req, res) => {
+    if (req.session.authenticated) {
+      const name = req.session.user.name;
+        res.render('index', { title: 'Dashboard', name: name });
+    } else {
+        res.render('index', { title: 'Dashboard', showLoginForm: true });
+    }
+});
+
+
+  // Logout route
+  router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error('Error destroying session', err);
+        return res.sendStatus(500);
+      }
+      res.redirect('/login');
+    });
+  });
+
+
+  router.get('/register', function (req, res, next) {
+    res.render('register', { title: 'Register' });
+
+  });
+
+  router.post('/register', async (req, res) => {
+    const { email, name, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const userid = uuidv4(); // Generate a unique userid using uuidv4()
+  
+    try {
+      await pool.query('INSERT INTO users (userid, email, name, password, role) VALUES ($1, $2, $3, $4, $5)', [userid, email, name, hashedPassword, 'guest']);
+      res.redirect('/');
+    } catch (error) {
+      console.error('Error inserting user:', error);
+      res.render('register', { title: 'Register', error: 'Error registering user' });
+    }
+  });
+
+  return router;
+}
+
+// module.exports = router;
